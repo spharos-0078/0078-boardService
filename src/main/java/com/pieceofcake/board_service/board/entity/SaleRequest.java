298ca1438e74e4b0b400d96e941d65618adec1f0
Eunseo <kendyjenny@naver.com>
2025-05-28 13:53:02 +0900

    [feat] board service 기본 설정

---
+++ src/main/java/com/pieceofcake/board_service/board/entity/SaleRequest.java
@@ -0,0 +1,4 @@
+package com.pieceofcake.board_service.board.entity;
+
+public class SaleRequest {
+}
---
+++ src/main/java/com/pieceofcake/board_service/board/enums/BoardState.java
@@ -0,0 +1,5 @@
+package com.pieceofcake.board_service.board.enums;
+
+public enum BoardState {
+    COMMENT_COMPLETED
+}
